012357ae2bbde3670f358f39d219b7289d3e7c6b
yukimatsuno <yuki.ama8@gmail.com>
2023-03-21 21:52:23 +0900

    Completed Display Individual Jobs

--- jobs/app.py
+++ jobs/app.py
@@ -29,9 +29,13 @@ def close_connection(exception):
         connection.close()
         return
 
-
 @app.route('/')
 @app.route('/jobs')
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job=job)
